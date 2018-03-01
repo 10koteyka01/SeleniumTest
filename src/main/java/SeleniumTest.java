44666e590832b441c37c43088f8ce5d3dfe793db
Konstantin Tihonenkov <tihonenkovk@mail.ru>
2018-03-01 17:37:47 +0700

    Сократил методы, избавился от большого конструктора в классе Search

--- src/main/java/SeleniumTest.java
+++ src/main/java/SeleniumTest.java
@@ -5,25 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import search.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeleniumTest {
-
-    private static Map<String, Search> searchMap = new HashMap<>();
+    private static List<Search> searchList = new ArrayList();
 
     static {
-        searchMap.put("yandex", new Search("yandex", "performance-lab", "//input[@name='text']", "//a[@href='http://www.performance-lab.ru/']"));
-        searchMap.put("google", new Search("google", "performance-lab", "//input[@title='Поиск']", "//a[@href='http://www.performance-lab.ru/']"));
-        searchMap.put("rambler", new Search("rambler", "performance-lab", "//input[@type='text']", "//a[@href='http://www.performance-lab.ru/']"));
+        searchList.add(new Search("yandex", "performance-lab"));
+        searchList.add(new Search("google", "performance-lab"));
+        searchList.add(new Search("rambler", "performance-lab"));
     }
 
     public static void main(String[] args){
         WebDriver driver = new ChromeDriver();
         Helper.driver = driver;
-        for(Map.Entry entry: searchMap.entrySet()){
-            Search searcher = (Search) entry.getValue();
-//            searcher.setQuery("performance-lab");
+        for(Search searcher : searchList){
             PerfSearch perfSearch = new PerfSearch(searcher.getSearchResult(), searcher.getSearcherName());
             perfSearch.openPerfSyte();
         }
--- src/main/java/search/Search.java
+++ src/main/java/search/Search.java
@@ -4,18 +4,30 @@ import Service.Helper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Search {
 
-    protected String query;
-    protected String searcherName;
-    protected String nameForFindResults;
-    protected String tagName;
+    private String query;
+    private String searcherName;
+    private WebElement element;
+    private Map<String, String> searchersMap;
 
-    public Search(String... parameters) {
-        searcherName = parameters[0];
-        query = parameters[1];
-        nameForFindResults = parameters[2];
-        tagName = parameters[3];
+    public Search(String searcherName, String query) {
+        this.searcherName = searcherName;
+        this.query = query;
+    }
+
+    public Search(String searcherName) {
+        this.searcherName = searcherName;
+    }
+
+    {
+        searchersMap = new HashMap();
+        searchersMap.put("yandex", "//input[@name='text']");
+        searchersMap.put("google", "//input[@title='Поиск']");
+        searchersMap.put("rambler", "//input[@type='text']");
     }
 
     public void setQuery(String query){
@@ -30,22 +42,35 @@ public class Search {
         return findElementsByTagName();
     }
 
-    protected WebElement findElementsByTagName(){
+    private WebElement findElementsByTagName(){
         inputQuery();
         Helper.switchWindow();
-        WebElement element = Helper.driver.findElement(By.xpath(tagName));
+        WebElement element = Helper.driver.findElement(By.xpath("//a[@href='http://www." + query + ".ru/']"));
         element.click();
         return element;
     }
 
-    protected void inputQuery(){
+    private void inputQuery(){
         Helper.driver.get("https://"+ searcherName + ".ru/");
-        WebElement element = Helper.driver.findElement(By.xpath(nameForFindResults));
+        element = Helper.driver.findElement(By.xpath(searchersMap.get(searcherName)));
         element.sendKeys(query);
-        if (searcherName.equals("rambler")){
-            element = Helper.driver.findElement(By.xpath("//button[@data-cerber-head='main::search::button']"));
-            element.click();
-        } else
-            element.submit();
+        startSearch();
+    }
+
+    private void startSearch(){
+        if (searcherName.equals("rambler"))
+            startSearchInBrowserWithButton();
+        else
+            startSearchInBrowserWithoutButton();
+    }
+
+    private void startSearchInBrowserWithButton(){
+        element = Helper.driver.findElement(By.xpath("//button[@data-cerber-head='main::search::button']"));
+        element.click();
+    }
+
+    private void startSearchInBrowserWithoutButton(){
+        element.submit();
     }
 }
+
